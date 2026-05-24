4548d1e485dde17421cc4b2943fb71a684bb1b64
Giovanni Foppiani <164873735+giovannifoppiani@users.noreply.github.com>
2026-05-24 15:12:26 +0300

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-//collego il service worker e controllo se è registrato
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('sw.js')
-    .then(reg => console.log('service worker registrato', reg))
-    .catch(err => console.log('service worker non registato', err));
-}
